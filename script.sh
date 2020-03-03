1554761f3d3dfeb80b6468f5c50da2320e883dde
Administrator <admin@example.com>
2020-03-03 10:05:19 +0800

    修正script docker tag的错误

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 docker login 49.235.21.109 -u admin -p Admin123
 version=2.1
 docker build  -t demo:${version} .
-docker tag demo:${version} 49.235.21.109/snapshot/demo:${version} .
+docker tag demo:${version} 49.235.21.109/snapshot/demo:${version}
 #docker build -f Dockerfile --tag ganmaotong:demo .
 #docker push ganmaotong:demo
 docker push 49.235.21.109/snapshot/demo:${version}
