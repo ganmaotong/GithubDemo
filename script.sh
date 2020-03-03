4eb3ad067ad52d3fb602e4a6a9fed1acfb3317fe
Administrator <admin@example.com>
2020-03-03 09:41:56 +0800

    修正script脚本，向harbor推送镜像

--- script.sh
+++ script.sh
@@ -1,2 +1,13 @@
-docker build -f Dockerfile --tag ganmaotong:demo .
-docker push ganmaotong:demo
+#!/bin/bash
+docker login 49.235.21.109 -u admin -p Admin123
+version=2.1
+docker build  -t demo:${version} .
+docker tag demo:${version} 49.235.21.109/snapshot/demo:${version} .
+#docker build -f Dockerfile --tag ganmaotong:demo .
+#docker push ganmaotong:demo
+docker push 49.235.21.109/snapshot/demo:${version}
+if [ $? -eq 0 ]; then
+ echo "push Success"
+else
+ echo "push failed"
+fi
